12a7f1619389ba9430f43c282f32b75bf52eaa58
Sidney-Bernardin <sidney.bernardin@gmail.com>
2025-11-19 19:44:11 -0500

    Refactored and added print function.

--- lua/tesser.lua
+++ lua/tesser.lua
@@ -1,34 +1,88 @@
-local M = {}
-local data = require(".data")
+local M = {
+    keys = "1234567890qwertyuiopasdfghjkl;zxcvbnm,./",
+    projects = {},
+}
 
-function M.setup() end
+--- Initialize data directory.
+local data_path = ("%s/tesser"):format(vim.fn.stdpath("data"))
+if vim.fn.isdirectory(data_path) == 0 then
+    vim.fn.mkdir(data_path)
+end
 
-function M.open(char)
+--- Returns current project's ID.
+function M.get_current_project_id()
     local cwd = vim.fn.getcwd(-1)
-    local project = data.load_project(cwd)
+    return ("%s/%s.json"):format(data_path, vim.fn.sha256(cwd))
+end
 
-    if not project[char] then
-        vim.notify(("Tesser: [%s] = %s"):format(char, project[char]), vim.log.levels.ERROR)
+--- Returns current project and it's ID.
+--- @return string project_id
+--- @return table<string, string> project
+function M.load_current_project()
+    local project_id = M.get_current_project_id()
+
+    if not M.projects[project_id] then
+        local _, project_json = pcall(vim.fn.readblob, project_id)
+        local ok, project = pcall(vim.json.decode, project_json)
+        M.projects[project_id] = ok and project or {}
+    end
+
+    return project_id, M.projects[project_id]
+end
+
+--- Saves project to disk.
+function M.save_project(project_id)
+    local project_file = assert(io.open(project_id, "w"))
+    project_file:write(vim.json.encode(M.projects[project_id]))
+    project_file:close()
+end
+
+--- Sets key to current file.
+--- @param key string
+function M.set(key)
+    local project_id, project = M.load_current_project()
+    project[key] = vim.fn.expand("%:.")
+    M.save_project(project_id)
+    vim.notify(("Tesser: [%s] set to %s"):format(key, project[key]))
+end
+
+--- Opens key's file.
+--- @param key string
+function M.open(key)
+    local _, project = M.load_current_project()
+
+    if not project[key] then
+        vim.notify(("Tesser: [%s] is empty"):format(key), vim.log.levels.ERROR)
         return
     end
 
-    vim.cmd(("e %s"):format(project[char]))
+    vim.cmd(("e %s"):format(project[key]))
 end
 
-function M.set(char)
-    local cwd = vim.fn.getcwd(-1)
-    local project = data.load_project(cwd)
-
-    project[char] = char and vim.fn.expand("%:.") or nil
-    data.save_project(cwd)
-
-    vim.notify(("Tesser: [%s] = %s"):format(char, project[char]))
+--- Clears key.
+--- @param key string
+function M.clear(key)
+    local project_id, project = M.load_current_project()
+    project[key] = nil
+    M.save_project(project_id)
+    vim.notify(("Tesser: [%s] cleared"):format(key))
 end
 
-function M.clear()
-    local cwd = vim.fn.getcwd(-1)
-    data.delete_project(cwd)
-    vim.notify(("Tesser: Cleared all from %s"):format("cwd"))
+--- Clears all keys.
+function M.clear_all()
+    local project_id = M.get_current_project_id()
+    M.projects[project_id] = nil
+    os.remove(project_id)
+    vim.notify("Tesser: Cleared all")
+end
+
+--- Prints keys.
+function M.print()
+    local _, project = M.load_current_project()
+
+    for key in M.keys:gmatch(".") do
+        print(("%s: %s"):format(key, project[key]))
+    end
 end
 
 return M
--- plugin/commands.lua
+++ plugin/commands.lua
@@ -1,3 +1,19 @@
-vim.api.nvim_create_user_command("TesserClear", function()
-    require("tesser").clear()
-end, { nargs = "*" })
+vim.api.nvim_create_user_command("TesserSet", function(args)
+    require("tesser").set(args.fargs[1])
+end, { nargs = 1 })
+
+vim.api.nvim_create_user_command("TesserOpen", function(args)
+    require("tesser").open(args.fargs[1])
+end, { nargs = 1 })
+
+vim.api.nvim_create_user_command("TesserClear", function(args)
+    require("tesser").clear(args.fargs[1])
+end, { nargs = 1 })
+
+vim.api.nvim_create_user_command("TesserClearAll", function()
+    require("tesser").clear_all()
+end, {})
+
+vim.api.nvim_create_user_command("TesserPrint", function()
+    require("tesser").print()
+end, {})
